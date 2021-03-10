7b2d9320b21bfafcfb169a6c37a5af34ccc0097c
ron93 <ronmkare1@gmail.com>
2021-03-10 15:04:42 +0300

    dag -- tsk_II : insert questions to db

--- dags/dags.py
+++ dags/dags.py
@@ -43,6 +43,8 @@ with DAG("stack_overflow_questions", default_args=default_args) as dag:
     )
 
     Task_II = PythonOperator(
+        task_id="insert_question_to_db",
+        python_callable=insert_question_to_db
     )
 
     Task_III = PythonOperator(
@@ -58,36 +60,3 @@ with DAG("stack_overflow_questions", default_args=default_args) as dag:
 
 Task_I >> Task_II >> Task_III >> Task_IV >> Task_V
 
-
-
-def call_api() -> dict:
-    '''get first 50 questions'''
-    question_url = Variable.get('STACK_OVERFLOW_QUESTION_URL')
-
-    today = datetime.now()
-    three_daya_ago = today - timedelta(days=7)
-    two_days_ago = today - timedelta(days=5)
-
-    payload = {
-        "fromdate" : int(datetime.timestamp(three_daya_ago)),
-        "todate": int(datetime.timestamp(two_days_ago)),
-        "sort" : "votes",
-        "site" : "stackoverflow",
-        "order" : "desc",
-        "tagged": Variable.get("TAG"),
-        "client_id" : Variable.get("STACK_OVERFLOW_CLIENT_ID"),
-        "client_secret" : Variable.get("STACK_OVERFLOW_KEY") 
-    }
-
-    response = request.get(question_url, params=payload ) 
-    
-    for question in response.json().get("items",[]):
-        yield {
-             "question_id": question["question_id"],
-            "title": question["title"],
-            "is_answered": question["is_answered"],
-            "link": question["link"],
-            "owner_reputation": question["owner"].get("reputation", 0),
-            "score": question["score"],
-            "tags": question["tags"],
-        }
